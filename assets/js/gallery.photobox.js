ba3ec19dac41de1d3b75238f968ec90a105e4159
eriksonik <e.halomov@glavpunkt.ru>
2018-10-28 02:10:36 +0300

    ver. 1.0.3

---
+++ assets/js/gallery.photobox.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function(){
+    jQuery('.photobox-gallery').photobox('a',{ time:0 });
+});
---
+++ models/ImageManager.php
@@ -0,0 +1,35 @@
+<?php namespace Eriks\Photobox\Models;
+
+use Model;
+
+/**
+ * ImageManager Model
+ */
+class ImageManager extends Model
+{
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'eriks_photobox_image_managers';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $attachMany = ['photos' => ['System\Models\File']];
+
+    public function getTotalImageAttribute(){
+        return count($this->photos);
+    }
+
+}
